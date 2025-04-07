77cb5d728faac44ad7aef0b0af9920fdf53f13fe
Emezao <emmezao@gmail.com>
2025-04-07 08:49:55 -0300

    Add files via upload

--- emezao/AULA_06/Exercicio_tintas_01.py
+++ emezao/AULA_06/Exercicio_tintas_01.py
@@ -1,14 +1,16 @@
 # Faça um programa para uma loja de tintas, que solicite o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 130,00. Informe ao usuário a quantidade de latas de tinta a serem compradas e o valor a ser pago.
-
+import math
 # Entrada de Dados
 
-tinta_18l = 130,00
-tinta_1l = 3
-n = 1
+area = float(input("informe o tamanho da área a ser pintada em metros quadrados: "))
 
 
 # Processamento de Dados
-metros = int(input('Informe em metros a área a ser pintada: ')) 
-Latas_tinta = metros + (tinta_18l*tinta_1l)
+litros_necessarios = area / 3
 
-print(f"compra ficou em R$: {Latas_tinta}")
+latas_necesasarias = math.ceil(litros_necessarios / 18)
+
+preco_total = latas_necesasarias * 130
+
+print(f"\nQuantidade de latas a serem compradas: {latas_necesasarias}")
+print(f"Valor total a ser pago: R${preco_total:.2f}")
